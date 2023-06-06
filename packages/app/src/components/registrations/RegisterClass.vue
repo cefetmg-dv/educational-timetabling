8466124313a86f818d4b2f42307ecfd1eb7d4716
wallaceketler <wallace-m-m-2011@hotmail.com>
2023-06-06 20:40:34 -0300

    inserção correta no json

--- packages/app/src/components/registrations/RegisterClass.vue
+++ packages/app/src/components/registrations/RegisterClass.vue
@@ -1,17 +1,22 @@
 <script>
 
     export default {
+        data(){
+            return{
+                formData: {
+                    name: '',
+                    abbreviation: ''
+                }
+            };
+        },
+
         methods: {
             HandleSubmit(e) {
                 e.preventDefault();
-                //console.log(this.name)
-                //console.log(this.abbreviation)
-                //data = {
-                //    name : this.name,
-                //    abbreviation : this.abbreviation
-                //}
-
-                window.salvarPalavra(this.name) 
+                var data = JSON.stringify(this.formData)
+                console.log(data)
+                
+                window.salvarArquivo(data)
             }
         }
     }
@@ -23,12 +28,12 @@
         <form class="registerClass-form" @submit="HandleSubmit">
             <div class="mb-3">
                 <label class="form-label">Nome da turma</label>
-                <input required v-model="name" type="text" class="form-control" aria-describedby="emailHelp"/>
+                <input required v-model="formData.name" type="text" class="form-control" aria-describedby="emailHelp"/>
                 
             </div>
             <div class="mb-3">
                 <label className="form-label">Abreviatura</label>
-                <input required v-model="abbreviation" type="text" class="form-control"/>
+                <input required v-model="formData.abbreviation" type="text" class="form-control"/>
             </div>
 
             <button type="submit" class="btn btn-primary">Cadastrar</button>
